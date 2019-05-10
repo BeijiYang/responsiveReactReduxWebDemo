0e04d03e5e4360703d9e62f60e379e0c6fa7ed99
BeijiYang <langyuyang@outlook.com>
2019-05-10 17:49:35 +0200

    add: card

--- RequestDemo/client/src/Card.js
+++ RequestDemo/client/src/Card.js
@@ -3,7 +3,13 @@ import './card.css'
 
 const Card = ({ name, email, phone, intro, url }) => (
   <div className="card">
-    Card
+    <img src='https://picsum.photos/300/300' className="card-picture" alt="card" />
+    <div className="card-info">
+      <div className="card-name">name: {name}</div>
+      <div className="card-email">email: {email}</div>
+      <div className="card-phone">phone: {phone}</div>
+      <div className="card-intro">intro: {intro}</div>
+    </div>
   </div>
 )
 
