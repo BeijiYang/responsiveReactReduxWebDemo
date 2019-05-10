6549d278750e6840d1e0cbbc0dc18ac3b9bdca9e
BeijiYang <langyuyang@outlook.com>
2019-05-10 18:39:38 +0200

    add: prototype checking

--- RequestDemo/client/src/Card.js
+++ RequestDemo/client/src/Card.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import './card.css'
 
 const Card = ({ name, email, phone, intro, url }) => (
   <div className="card">
-    <img src='https://picsum.photos/300/300' className="card-picture" alt="card" />
+    <img src={url} className="card-picture" alt="card" />
     <div className="card-info">
       <div className="card-name">name: {name}</div>
       <div className="card-email">email: {email}</div>
@@ -12,5 +13,14 @@ const Card = ({ name, email, phone, intro, url }) => (
     </div>
   </div>
 )
+
+Card.propTypes = {
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
+  phone: PropTypes.string.isRequired,
+  intro: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
+
 
 export default Card
