5c8d72fe6714e5e5dde6612045c7475a1875d3c4
BeijiYang <langyuyang@outlook.com>
2019-05-12 00:17:48 +0200

    refactor: add a constant & getIntSmallerThan func

--- RequestDemo/server/index.js
+++ RequestDemo/server/index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const TOTAL_PAGE_NUM = 10
+const getIntSmallerThan = num => Math.floor(Math.random() * num)
+
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
@@ -17,17 +20,17 @@ app.post('/companies', (req, res) => {
 
   const getData = length => Array.from({ length }, () => ({
     id: Math.random(),
-    name: `Company No.${Math.floor(Math.random() * 1000)}`,
+    name: `Company No.${getIntSmallerThan(1000)}`,
     email: 'email@email.com',
     phone: '0123456789',
     intro: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-    url: `https://picsum.photos/300?random=${Math.floor(Math.random() * 10)}`,
+    url: `https://picsum.photos/300?random=${getIntSmallerThan(10)}`,
   }))
 
   const companies = getData(CARDS_PER_PAGE)
   res.send({
     companies,
-    totalPageNum: 10
+    totalPageNum: TOTAL_PAGE_NUM
   })
 })
 
