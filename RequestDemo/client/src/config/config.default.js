9288736212f124cd3f4241f98e34f72805ac0963
BeijiYang <langyuyang@outlook.com>
2019-05-11 18:14:57 +0200

    add: move colors & constants in config

--- RequestDemo/client/src/config/config.default.js
+++ RequestDemo/client/src/config/config.default.js
@@ -1,5 +1,18 @@
 export default {
   API: {
     companies: 'http://localhost:3001/companies'
+  },
+  colors: {
+    DARK_PRIMARY_COLOR: '#0097A7',
+    DEFAULT_PRIMARY_COLOR: '#00BCD4',
+    LIGHT_PRIMARY_COLOR: '#B2EBF2',
+    TEXT_PRIMARY_COLOR: '#FFFFFF',
+    ACCENT_COLOR: '#FF5722',
+    PRIMARY_TEXT_COLOR: '#212121',
+    SECONDARY_TEXT_COLOR: '#757575',
+    DIVIDER_COLOR: '#BDBDBD',
+  },
+  constants: {
+    CARDS_PER_PAGE: 12
   }
 }
