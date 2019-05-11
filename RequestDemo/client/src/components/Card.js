ddd344965a95d73c6216230a58b832f4d7f5f172
BeijiYang <langyuyang@outlook.com>
2019-05-11 18:10:41 +0200

    add: img loading using react hook

--- RequestDemo/client/src/components/Card.js
+++ RequestDemo/client/src/components/Card.js
@@ -1,18 +1,36 @@
-import React from 'react'
-import PropTypes from 'prop-types';
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleNotch } from '@fortawesome/free-solid-svg-icons'
 import '../styles/card.css'
 
-const Card = ({ name, email, phone, intro, url }) => (
-  <div className="card">
-    <img src={url} className="card-picture" alt="card" />
-    <div className="card-info">
-      <div className="card-name">Name: {name}</div>
-      <div className="card-email">Email: {email}</div>
-      <div className="card-phone">Phone: {phone}</div>
-      <div className="card-intro">Intro: {intro}</div>
+const Card = ({ name, email, phone, intro, url }) => {
+  const [showPlaceHolder, setShowPlaceHolder] = useState(true)
+  const handleImageLoaded = () => { setShowPlaceHolder(false) }
+  const handleImageLoadedError = () => { setShowPlaceHolder(true) }
+
+  return (
+    <div className="card">
+      <img
+        src={url}
+        className="card-picture"
+        alt="card"
+        onLoad={handleImageLoaded}
+        onError={handleImageLoadedError}
+        style={{ display: showPlaceHolder ? 'none' : 'block' }}
+      />
+      <div className="loading-picture" style={{ display: showPlaceHolder ? 'block' : 'none' }} >
+        <FontAwesomeIcon icon={faCircleNotch} className="loading-picture-icon" spin size="4x" />
+      </div>
+      <div className="card-info">
+        <div className="card-name">Name: {name}</div>
+        <div className="card-email">Email: {email}</div>
+        <div className="card-phone">Phone: {phone}</div>
+        <div className="card-intro">Intro: {intro}</div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 Card.propTypes = {
   name: PropTypes.string.isRequired,
