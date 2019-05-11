154ebd3b07f00c32cca569c99842606cd7e44ff5
BeijiYang <langyuyang@outlook.com>
2019-05-11 18:06:47 +0200

    add: loading card as placeholder

---
+++ RequestDemo/client/src/components/LoadingCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../styles/loading.css'
+
+const LoadingCard = () => (
+  <div className="loading">
+    <div className="loading-picture" />
+    <div className="loading-info">
+      <div className="loading-text-short" />
+      <div className="loading-text-long" />
+      <div className="loading-text-short" />
+      <div className="loading-text-long" />
+    </div>
+  </div>
+)
+
+
+export default LoadingCard
