4f3da0ea41e1759275462e10c3e2bc2129173343
BeijiYang <langyuyang@outlook.com>
2019-05-11 18:11:27 +0200

    refactor: use button on mobile & stepper on web

--- RequestDemo/client/src/components/Content.js
+++ RequestDemo/client/src/components/Content.js
@@ -1,64 +1,133 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import Card from './Card'
+import LoadingCard from './LoadingCard'
 import ProgressStepper from './ProgressStepper'
 import config from '../config/config'
+import Button from './CustomButton';
 import '../styles/content.css'
 
-const { API: { companies } } = config
+const {
+  API: { companies: companiesUrl },
+  constants: { CARDS_PER_PAGE },
+} = config
 
 export default class Content extends Component {
-
   state = {
     companies: [],
+    companiesOnCurrentPage: Array(CARDS_PER_PAGE).fill(null),
     activeStep: 0,
-    totalPageNum: 0
+    totalPageNum: 0,
   }
 
   componentDidMount() {
-    this.fetchCompanies(0)
+    this.showNewCompanies(0)
   }
 
-  fetchCompanies = (pageIndex) => axios.post(companies, pageIndex)
-    .then(res => res.data)
-    .then(data => {
-      const { companies, totalPageNum } = data
-      this.setState({
-        companies: [...this.state.companies, ...companies],
-        totalPageNum: totalPageNum
-      })
-    })
+  fetcher = pageIndex => axios.post(companiesUrl, pageIndex).then(res => res.data)
 
+  fetchNewCompanies = async (pageIndex) => {
+    const resData = await this.fetcher(pageIndex)
+    const { companies, totalPageNum } = resData
+    const newAllCompanies = [...this.state.companies, ...companies]
+    this.setState({
+      companies: newAllCompanies,
+      totalPageNum: totalPageNum,
+    })
+    return [companies, newAllCompanies]
+  }
+
+  showAllCompanies = async (pageIndex) => {
+    const [, newAllCompanies] = await this.fetchNewCompanies(pageIndex)
+    this.setState({ companiesOnCurrentPage: newAllCompanies })
+  }
+
+  showSavedCompanies = (pageIndex) => {
+    const { state: { companies } } = this
+    const getPosition = index => index * CARDS_PER_PAGE
+    const startPosition = getPosition(pageIndex)
+    const endPosition = getPosition(pageIndex + 1)
+    const companiesToShow = companies.slice(startPosition, endPosition)
+    this.setState({ companiesOnCurrentPage: companiesToShow })
+  }
   /**
    * @param direction {Number} 1 for Next and -1 for Back
    */
-  handleClick = (direction) => {
+  handleClickStepper = (direction) => {
+    window.scrollTo(0, 0)
+    const newActiveSetp = this.updateActiveStep(direction)
+    this.switchPageForWeb(newActiveSetp)
+  }
+  /**
+   * @param direction {Number} 1 for Next and -1 for Back
+   */
+  updateActiveStep = (direction) => {
     const { activeStep } = this.state
     const newActiveSetp = activeStep + direction
     this.setState({ activeStep: newActiveSetp })
-    this.fetchCompanies(newActiveSetp)
+    return newActiveSetp
   }
 
+  switchPageForWeb = (pageIndex) => {
+    const savedPages = this.state.companies.length / CARDS_PER_PAGE
+    const needToFetch = pageIndex >= savedPages
+
+    needToFetch
+      ? this.showNewCompanies(pageIndex)
+      : this.showSavedCompanies(pageIndex)
+  }
+
+  showNewCompanies = async (pageIndex) => {
+    const [newCompanies,] = await this.fetchNewCompanies(pageIndex)
+    this.setState({ companiesOnCurrentPage: newCompanies })
+  }
+
+  handleClickButton = () => {
+    const direction = 1 // next page
+    const newActiveSetp = this.updateActiveStep(direction)
+    this.showAllCompanies(newActiveSetp)
+  }
+
+  showCards = (companies) => companies.map(
+    (company, index) => {
+      if (!company) return <LoadingCard key={index} />
+      const { id, ...info } = company
+      return <Card
+        key={id}
+        {...info}
+      />
+    }
+  )
+
   render() {
-    const cards = this.state.companies.map(
-      company => {
-        const { id, ...info } = company
-        return <Card
-          key={id}
-          {...info}
-        />
-      }
-    )
+    const {
+      state: { companiesOnCurrentPage, totalPageNum, activeStep },
+      showCards,
+      handleClickStepper,
+      handleClickButton
+    } = this
+
+    const cards = showCards(companiesOnCurrentPage)
 
     return (
       <div className='content'>
         {cards}
-        <ProgressStepper
-          totalPageNum={this.state.totalPageNum}
-          activeStep={this.state.activeStep}
-          handleNext={() => this.handleClick(1)}
-          handleBack={() => this.handleClick(-1)}
-        />
+        <div className='content-stepper'>
+          <ProgressStepper
+            totalPageNum={totalPageNum}
+            activeStep={activeStep}
+            handleNext={() => handleClickStepper(1)}
+            handleBack={() => handleClickStepper(-1)}
+          />
+        </div>
+        <div className='content-mobile'>
+          <Button
+            handleClick={() => handleClickButton(1)}
+            disabled={totalPageNum === activeStep + 1}
+          >
+            Next Page
+          </Button>
+        </div>
       </div>
     )
   }
