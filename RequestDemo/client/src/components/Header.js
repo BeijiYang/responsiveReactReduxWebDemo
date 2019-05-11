f343b7b234d687b4fdf45b29793431a3f8421077
BeijiYang <langyuyang@outlook.com>
2019-05-11 18:07:47 +0200

    add: login font icon

--- RequestDemo/client/src/components/Header.js
+++ RequestDemo/client/src/components/Header.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons'
 import '../styles/header.css'
 
 const Header = () => (
   <header className='header'>
     <span className='header-title'>Companies</span>
+    <FontAwesomeIcon icon={faSignInAlt} size="lg" className="header-login-icon" />
     <span className="header-login">Login</span>
   </header>
 )
