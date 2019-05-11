79facabb4751219e686f6f46e15586a361da95b3
BeijiYang <langyuyang@outlook.com>
2019-05-11 18:08:16 +0200

    add: footer content

--- RequestDemo/client/src/components/Footer.js
+++ RequestDemo/client/src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import '../styles/footer.css'
 
 const Footer = () => (
   <footer className='footer'>
-    Footer
+    <span className='footer-self-intro'>I'm Yujie Wang, a front end developer.</span>
+    <a href="https://github.com/BeijiYang" className='footer-link'>
+      <FontAwesomeIcon icon={faGithub} size="lg" className="footer-icon" />
+      <div>About Me</div>
+    </a>
   </footer>
 )
 
